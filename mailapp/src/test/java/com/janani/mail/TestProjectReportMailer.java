c99bcead6a3967ec75330041b83a5ecaaeac11ef
Janani <jananise@gmail.com>
2017-01-27 15:16:37 +0530

    Initial Commit

--- mailapp/src/test/java/com/janani/mail/TestProjectReportMailer.java
+++ mailapp/src/test/java/com/janani/mail/TestProjectReportMailer.java
@@ -1,16 +1,21 @@
 package com.janani.mail;
 
+import java.util.logging.Logger;
+
 import com.janani.util.MailUtil;
 
 public class TestProjectReportMailer {
+	
+	private final static Logger LOGGER = Logger.getLogger(TestProjectReportMailer.class.getName());
 	public static void main(String[] args) {
 
 		String content = ProjectReportMailer.getContent();
 
 		System.out.println(content.toString());
+		//LOGGER.info(content.toString());
 
 		String subject = "Reg - Order Status";
-		MailUtil.sendHtmlMail(subject, content);
+		//MailUtil.sendHtmlMail(subject, content);
 	}
 
 }
